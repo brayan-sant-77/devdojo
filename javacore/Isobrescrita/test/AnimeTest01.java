2256604d85777b175fcb4d6f68f15bb20108844a
Brayan Oliveira dos Santos <bo257980@gmail.com>
2026-06-05 19:12:55 -0300

    aprendendo sobre método toString

---
+++ javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.javacore.Isobrescrita.test;
+
+import academy.devdojo.javacore.Isobrescrita.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+           Anime anime = new Anime("Bleach");
+
+        System.out.println(anime);
+    }
+}
