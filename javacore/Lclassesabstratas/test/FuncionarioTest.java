3a91c5be102fbf794e1a1251c81162b027292adc
Brayan Oliveira dos Santos <bo257980@gmail.com>
2026-06-15 16:28:21 -0300

    introdução à classes abstratas

---
+++ javacore/Lclassesabstratas/test/FuncionarioTest.java
@@ -0,0 +1,14 @@
+package academy.devdojo.javacore.Lclassesabstratas.test;
+
+import academy.devdojo.javacore.Lclassesabstratas.domain.Desenvolvedor;
+import academy.devdojo.javacore.Lclassesabstratas.domain.Gerente;
+
+public class FuncionarioTest {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Midorya", 5000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Hollow Knight", 13000);
+
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
+}
