ecb5d5838fc69f2a00534424c38e1e88fb7056da
Brayan Oliveira dos Santos <bo257980@gmail.com>
2026-06-19 15:58:15 -0300

    Parâmetros polimórficos

--- javacore/Npolimorfismo/service/CalculadoraImposto.java
+++ javacore/Npolimorfismo/service/CalculadoraImposto.java
@@ -1,22 +1,13 @@
 package academy.devdojo.javacore.Npolimorfismo.service;
 
-import academy.devdojo.javacore.Npolimorfismo.domain.Celular;
-import academy.devdojo.javacore.Npolimorfismo.domain.Videogame;
+import academy.devdojo.javacore.Npolimorfismo.domain.Produto;
 
 public class CalculadoraImposto {
-    public static void calculadoraImpostoCelular(Celular celular) {
-        System.out.println("Relátorio de imposto do celular:");
-        double imposto = celular.calcularImposto();
-        System.out.println("Celular: " + celular.getNome());
-        System.out.println("Valor: " + celular.getValor());
-        System.out.println("Valor do imposto: " + imposto);
-    }
-
-    public static void calculadoraImpostoVideoGame(Videogame videogame) {
-        System.out.println("Relátorio de imposto do videogame:");
-        double imposto = videogame.calcularImposto();
-        System.out.println("VideoGame: " + videogame.getNome());
-        System.out.println("Valor: " + videogame.getValor());
-        System.out.println("Valor do imposto: " + imposto);
+    public static void calcularImposto(Produto produto) {
+        System.out.println("Relatório de imposto do produto");
+        double imposto = produto.calcularImposto();
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Valor: " + produto.getValor());
+        System.out.println("Imposto: " + imposto);
     }
 }
--- javacore/Npolimorfismo/test/ProdutoTest.java
+++ javacore/Npolimorfismo/test/ProdutoTest.java
@@ -1,18 +1,21 @@
 package academy.devdojo.javacore.Npolimorfismo.test;
 
 import academy.devdojo.javacore.Npolimorfismo.domain.Celular;
+import academy.devdojo.javacore.Npolimorfismo.domain.Televisao;
 import academy.devdojo.javacore.Npolimorfismo.domain.Videogame;
 import academy.devdojo.javacore.Npolimorfismo.service.CalculadoraImposto;
 
 public class ProdutoTest {
     public static void main(String[] args) {
         Celular celular = new Celular("Samsung S25", 5000);
-        CalculadoraImposto.calculadoraImpostoCelular(celular);
-
-        System.out.println("--------------------");
-
         Videogame videogame = new Videogame("PS5", 4000);
-        CalculadoraImposto.calculadoraImpostoVideoGame(videogame);
+        Televisao tv = new Televisao("Samsung 50\" ", 4500);
+
+        CalculadoraImposto.calcularImposto(celular);
+        System.out.println("--------------------");
+        CalculadoraImposto.calcularImposto(videogame);
+        System.out.println("--------------------");
+        CalculadoraImposto.calcularImposto(tv);
 
     }
 }
