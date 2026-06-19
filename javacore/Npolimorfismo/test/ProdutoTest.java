37ec1a51f4519a0b8a4af553be015c4cff25b28b
Brayan Oliveira dos Santos <bo257980@gmail.com>
2026-06-19 13:49:38 -0300

    Preparando o terreno para o polimorfismo

--- javacore/Npolimorfismo/test/ProdutoTest.java
+++ javacore/Npolimorfismo/test/ProdutoTest.java
@@ -1,22 +1,18 @@
 package academy.devdojo.javacore.Npolimorfismo.test;
 
 import academy.devdojo.javacore.Npolimorfismo.domain.Celular;
-import academy.devdojo.javacore.Npolimorfismo.domain.Produto;
 import academy.devdojo.javacore.Npolimorfismo.domain.Videogame;
-
+import academy.devdojo.javacore.Npolimorfismo.service.CalculadoraImposto;
 
 public class ProdutoTest {
     public static void main(String[] args) {
-        Produto produto = new Celular("Samsung S25", 5000);
-        System.out.println(produto.getNome());
-        System.out.println(produto.getValor());
-        System.out.println(produto.calcularImposto());
+        Celular celular = new Celular("Samsung S25", 5000);
+        CalculadoraImposto.calculadoraImpostoCelular(celular);
 
-        System.out.println("--------------");
+        System.out.println("--------------------");
 
-        Produto produto2 = new Videogame("PS5", 4500);
-        System.out.println(produto2.getNome());
-        System.out.println(produto2.getValor());
-        System.out.println(produto2.calcularImposto());
+        Videogame videogame = new Videogame("PS5", 4000);
+        CalculadoraImposto.calculadoraImpostoVideoGame(videogame);
+
     }
 }
