f24fcbd0e06a5436e6a25c999dea9a61e9a27e41
Brayan Oliveira dos Santos <bo257980@gmail.com>
2026-06-21 16:32:49 -0300

    Aprendendo sobre errors

---
+++ javacore/Oexception/test/StackOverflowTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.javacore.Oexception.test;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+        recursividade();
+    }
+    public static void recursividade() {
+        recursividade();
+    }
+
+}
